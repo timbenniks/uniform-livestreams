c285bc44412e0f0c06c0ec5e35d14c357e30b006
Tim Benniks <tbenniks@gmail.com>
2023-01-25 17:23:26 +0100

    header home link

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,15 @@
 const Header = () => (
   <header className="bg-black sticky top-0">
     <div className="flex justify-between items-center px-8 xl:px-0 max-w-7xl m-auto h-20">
-      <img
-        src="./logo.png"
-        alt="Uniform"
-        width="130"
-        height="34"
-        className="block"
-      />
+      <a href="/">
+        <img
+          src="./logo.png"
+          alt="Uniform"
+          width="130"
+          height="34"
+          className="block"
+        />
+      </a>
       <ul className="flex space-x-6 text-sm underline items-center">
         <li>
           <a
