a88f531d659bc6bfe7d5e7f1988c0b467de70066
Tim Benniks <tbenniks@gmail.com>
2023-03-16 11:44:29 +0100

    Update playlist.ts

--- pages/api/playlist.ts
+++ pages/api/playlist.ts
@@ -1,28 +1,28 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+export function toVideo(video, fromPlaylist) {
+  return {
+    date: video.snippet.publishedAt,
+    title: video.snippet.title,
+    description: video.snippet.description,
+    image: video.snippet.thumbnails?.high?.url.replace("hqdefault", "maxresdefault"),
+    videoId: fromPlaylist ? video.snippet.resourceId.videoId : video.id.videoId ? video.id.videoId : video.id
+  }
+}
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { id, count } = req.query
   const data = await fetch(`https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${id}&key=${process.env.YOUTUBE_KEY}&maxResults=${count || 5}`)
 
   const result = await data.json()
+  let response;
 
-  const videos = result.items.map(video => {
-    return {
-      date: video.snippet.publishedAt,
-      title: video.snippet.title,
-      description: video.snippet.description,
-      image: video.snippet.thumbnails.maxres.url,
-      position: video.snippet.position,
-      videoId: video.snippet.resourceId.videoId
-    }
-  })
+  if (result.error) {
+    response = result.error
+  }
+  else {
+    response = result.items.map(video => (toVideo(video, true))).filter((video) => video.title !== 'Deleted video')
+  }
 
-  videos.sort(function (a, b) {
-    const bDate: any = new Date(b.date)
-    const aDate: any = new Date(a.date)
-
-    return bDate - aDate;
-  });
-
-  res.status(200).json({ videos })
+  res.status(response.code || 200).json({ result: response })
 }
